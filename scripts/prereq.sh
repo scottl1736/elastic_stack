97c6401d7b9695a51f0a1be28353315e4cbb13c3
scottl1736 <lynch@packetengineer.com>
2019-09-06 18:37:36 +0000

    Update prereq.sh
    added to pull from my github

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -3,7 +3,7 @@ if [ ! -d /opt/elastic_stack ];
 then
   echo "Cloning elastic_stack repo"
   cd /opt
-  git clone https://github.com/HASecuritySolutions/elastic_stack.git
+  git clone https://github.com/scottl1736/elastic_stack.git
   chown -R ${SUDO_USER} /opt/elastic_stack
 fi
 if grep docker /etc/group | grep -q ${SUDO_USER}
