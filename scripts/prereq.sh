a084f3cdd53c3ffe9bd654b4b4311a347a407d93
scottl1736 <lynch@packetengineer.com>
2019-09-06 18:24:11 +0000

    Update prereq.sh

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -1,52 +1,10 @@
 #!/bin/bash
-VERSION=`cat /etc/lsb-release | grep RELEASE | cut -d"=" -f2`
-if [ $VERSION = "18.04" ]; then
-  DOCKER="docker-ce"
-else
-  DOCKER="docker-ce"
-fi
-apt update
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing git"
-  apt install -y git
-else
-  echo "Git is already installed"
-fi
-if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing curl"
-  apt install -y curl
-else
-  echo "Curl is already installed"
-fi
-
 if [ ! -d /opt/elastic_stack ];
 then
   echo "Cloning elastic_stack repo"
   cd /opt
-  git clone https://github.com/HASecuritySolutions/elastic_stack.git
+  git clone https://github.com/scottl1736/elastic_stack.git
   chown -R ${SUDO_USER} /opt/elastic_stack
-fi
-if grep -q 'deb \[arch=amd64\] https://download.docker.com/linux/ubuntu' /etc/apt/sources.list
-then
-  echo "Docker software repository is already installed"
-else
-  echo "Docker software repository is not installed. Installing"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  if grep -q 'deb \[arch=amd64\] https://download.docker.com/linux/ubuntu' /etc/apt/sources.list
-  then
-    echo "Docker software repository is now installed"
-  fi
-fi
-if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing docker"
-  sudo apt-get install -y $DOCKER
-else
-  echo "Docker is already installed"
 fi
 if grep docker /etc/group | grep -q ${SUDO_USER}
 then
